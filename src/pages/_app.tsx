c426e79d68b156f665ff1e22beb2b7a708cd6d4a
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-22 12:36:38 -0300

    :wrench: chore: integrate application with reat-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
 
 import { theme } from '../styles/theme';
 
@@ -12,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
-const queryClient = new QueryClient();
+
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
